16645f056e8ca2380a6b15453c6f975c1056de70
LucianoRodrigues2026 <lucianorosriguesdasil.2018@gmail.com>
2026-05-23 21:33:45 -0300

    aula11

---
+++ aula11/ex009.js
@@ -0,0 +1,6 @@
+var numero = 68
+if (numero > 60) {
+  console.log('vc ultrapaçou o limite de velocidade permitida')
+}else{
+  console.log('vc esta dentrodo limite de velocidade permitido')
+}
---
+++ aula11/if.js
@@ -0,0 +1,6 @@
+var velocidade = 60
+console.log(`a sua velocidade e de ${velocidade}`)
+if (velocidade > 60) {
+  console.log('potanto vc foi multado em 1500 reais')
+}
+//condição simples
