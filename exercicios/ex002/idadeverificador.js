31f9a05afeff9399404a66c53f29b3bdc59e0ed5
LucianoRodrigues2026 <lucianorosriguesdasil.2018@gmail.com>
2026-05-26 05:18:57 -0300

    js ex002

--- exercicios/ex002/idadeverificador.js
+++ exercicios/ex002/idadeverificador.js
@@ -1,18 +1,46 @@
-var data = new Date()
-var anoAtual = data.getFullYear()
 
-function clicou(){
-  var sexo = document.getElementsByName('genero')
-
+function clicou() {
+  var data = new Date()
+  var anoAtual = data.getFullYear()
   var idade = document.getElementById('Idade')
   var idadeAno = Number(idade.value)
   var idadeAtual = anoAtual - idadeAno
   var mostra = document.getElementById('resultado')
-  mostra.innerHTML = `<h1>Você tem ${idadeAtual}</h1>`
 
-  //if (){
-
+  if (idade.value.length == 0 || idadeAno > anoAtual){
+    window.alert('calma velinho preencha os dados corretamente')
+    return
+  }
   
 
+  var sexo = document.getElementsByName('genero')
+  var genero = ""
+  if (sexo[0].checked && idadeAtual <= 12) {
+    genero = "Menina"
+    mostra.innerHTML = `<h1>Você e uma ${genero} de ${idadeAtual} anos</h1>`
+  }else if (sexo[0].checked && idadeAtual <= 25){
+    genero = "Mulher"
+    mostra.innerHTML = `<h1>Você e uma jovem ${genero} de ${idadeAtual} anos</h1>`
+  }else if (sexo[0].checked && idadeAtual <= 60){
+    genero = "Mulher"
+    mostra.innerHTML = `<h1>Você e uma ${genero} de ${idadeAtual} anos</h1>`
+  }else if (sexo[0].checked && idadeAtual > 60){
+    genero = "Mulher"
+    mostra.innerHTML = `<h1>Você e uma senhora${genero} de ${idadeAtual} anos</h1>`
+  }else{
+    if (sexo[1].checked && idadeAtual <= 12) {
+      genero = "Menino"
+      mostra.innerHTML = `<h1>Você e uma ${genero} de ${idadeAtual} anos</h1>`
+    }else if (sexo[0].checked && idadeAtual <= 25){
+    genero = "Mulher"
+    mostra.innerHTML = `<h1>Você e uma jovem ${genero} de ${idadeAtual} anos</h1>`
+    }else if (sexo[0].checked && idadeAtual <= 60){
+      genero = "Mulher"
+      mostra.innerHTML = `<h1>Você e uma ${genero} de ${idadeAtual} anos</h1>`
+    }else if (sexo[0].checked && idadeAtual > 60){
+      genero = "Mulher"
+      mostra.innerHTML = `<h1>Você e uma senhora${genero} de ${idadeAtual} anos</h1>`
+    }
+  }
+  
 }
-
