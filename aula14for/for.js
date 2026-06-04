af7b2609a100f89dd58699bc9e43b691c7dabd42
LucianoRodrigues2026 <lucianorosriguesdasil.2018@gmail.com>
2026-06-04 05:16:50 -0300

    variavel composta

--- aula14for/for.js
+++ aula14for/for.js
@@ -1,5 +1,11 @@
-for (var c = 1; c <= 5; c++){
+/*for (var c = 1; c <= 5; c++){
   console.log(c)
 
 }
-console.log('FIM')
+console.log('FIM')*/
+let vetorArry = ["", 1 , 2 , 3, 5 , 4 , 6 ]
+for (let pos=0; pos < vetorArry.length; pos++){
+  console.log(`objeto ${vetorArry[pos]}`)
+
+}
+console.log(vetorArry)
