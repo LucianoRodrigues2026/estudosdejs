3103a894b28cbba9e4e1d987283ac8d918142fff
LucianoRodrigues2026 <lucianorosriguesdasil.2018@gmail.com>
2026-06-05 05:15:16 -0300

    arry var compostas

--- aula15/var composta.js
+++ aula15/var composta.js
@@ -1,5 +1,9 @@
 var vetor = [1,2,3,4,5,6,7,8,9]
-for(var pos = 0; pos < vetor.length; pos = 1){
+for(var pos = 0; pos < vetor.length; pos ++){
   console.log(`na posição ${pos}, esta o número ${vetor[pos]}`)
 
 }
+
+  for(var pos in vetor){
+    console.log(`na posição ${pos} dentro do arry esta esta o nùmero ${vetor[pos]} `)
+  }
