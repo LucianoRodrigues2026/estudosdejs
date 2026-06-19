428fe780423e1fb9f70df71824b29028b225c0cc
LucianoRodrigues2026 <lucianorosriguesdasil.2018@gmail.com>
2026-06-19 05:22:20 -0300

    fundo

---
+++ projeto2/fondo.js
@@ -0,0 +1,9 @@
+
+function fundo(){
+  let cores = ['#e3c0d3', '#95818d', '#576066', '#2c514c', '#122932']
+  let botão = document.getElementById('cor')
+  let corpo = document.getElementsByClassName('body')
+  corpo.style.backgroundColor = [4]
+  
+
+}
