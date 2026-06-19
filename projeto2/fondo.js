5e933e75c1117001e7bf753f08b1dbd91c063b73
LucianoRodrigues2026 <lucianorosriguesdasil.2018@gmail.com>
2026-06-19 18:59:38 -0300

    pit

--- projeto2/fondo.js
+++ projeto2/fondo.js
@@ -1,9 +1,20 @@
+ let cores = ['#e3c0d3', '#95818d', '#576066', '#2c514c', '#122932']
+ let começo = 0
 
 function fundo(){
-  let cores = ['#e3c0d3', '#95818d', '#576066', '#2c514c', '#122932']
-  let botão = document.getElementById('cor')
-  let corpo = document.getElementsByClassName('body')
-  corpo.style.backgroundColor = [4]
-  
+  let corpo = document.getElementById('corpo')
+  //let botao = document.getElementByI('cor')
+
+  corpo.style.backgroundColor = cores[começo]
+  //cores.style.backgroundColor = cores[começo]
+
+  começo ++
+
+  if (começo >= cores.length){
+    começo = 0
+  }else {
+    começo == começo
+
+  }
 
 }
